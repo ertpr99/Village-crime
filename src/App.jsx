0805514a7d74a421a46547f4789d101b3097624e
ertpr99 <ert.pr99@gmail.com>
2026-04-14 21:43:27 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,7 @@
-
+export default function App() {
+  return (
+    <div style={{color:"white", background:"#111", height:"100vh", display:"flex", justifyContent:"center", alignItems:"center"}}>
+      <h1>Village Game Lobby Ready 🚀</h1>
+    </div>
+  );
+}
